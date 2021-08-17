9ee02c08ae8d09355237128269c7589114e1463b
DK_028 <deka.ganteng028@gmail.com>
2021-08-17 11:09:36 +0700

    Test commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+// @ts-check
 import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
   render() {
-    return <h1> Bruh </h1>;
+    return (
+      <div className="bg">
+        <div className="center">
+          <h1>TEST</h1>
+        </div>
+      </div>
+    );
   }
 }
